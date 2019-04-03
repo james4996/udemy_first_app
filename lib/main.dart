15c55342d7698c0f35351853d50edcaf28499768
James <james@tantami.co.uk>
2019-04-03 19:44:42 +0100

    Session Completed
    Completed upto session 28

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class DemoApp extends StatelessWidget {
-  build(context) {
-    return MaterialApp();
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Add Product'),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              ),
+            ],
+          )),
+    );
   }
 }
